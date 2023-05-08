0e827eca6ab70c9c607b8ecb03a6959f226124c8
k-seungri <sr930315@naver.com>
2023-05-08 16:32:24 +0900

    20230503 - Delete directory

--- moble_wedding/polls/urls.py
+++ moble_wedding/polls/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
+    # path("polls/", include("polls.urls")),
+    path("polls/", admin.views.index),
     path("admin/", admin.site.urls),
 ]
