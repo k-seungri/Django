9f10d857c1368e9d2ac179c2a4eccf21128fc091
k-seungri <sr930315@naver.com>
2023-05-24 17:59:30 +0900

    20230523 - chapter11

--- ch99/mysite/views.py
+++ ch99/mysite/views.py
@@ -3,6 +3,9 @@ from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
+from django.contrib.auth.mixins import AccessMixin
+from django.views.defaults import permission_denied
+
 
 #--- Homepage
 class HomeView(TemplateView):
@@ -18,4 +21,15 @@ class UserCreateView(CreateView):
 
 class UserCreateDoneTV(TemplateView):
     template_name = 'registration/register_done.html'
+
+
+class OwnerOnlyMixin(AccessMixin):
+    raise_exception = True
+    permission_denied_message = "Owner only can update/delete the object"
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.request.user != self.object.owner:
+            self.handle_no_permission()
+        return super().get(request, *args, **kwargs)
 
